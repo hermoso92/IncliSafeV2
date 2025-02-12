5b36d5d565ffa48a8264619200892af0747d2177
antoniohermoso9 <38606645-antoniohermoso9@users.noreply.replit.com>
2025-02-12 04:48:50 +0000

    User checkpoint: Add JWT Bearer authentication, MudBlazor UI component, Entity Framework Core tools, and iText7 for PDF generation.  Update project dependencies.

--- BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 1 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 2 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 3 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 4 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 5 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 6 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 7 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 8 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 9 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/home/runner/Blazor/BlazorApp/_Imports.razor"
+#line 10 "/home/runner/workspace/BlazorApp/_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
